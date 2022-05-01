1e3e06c93ce73f80c2b06ac68f04261ea2068096
snehasonwane <snehasonwane01@gmail.com>
2022-05-01 22:54:10 +0530

    check emp present/Absent

---
+++ ConsoleApp1/ConsoleApp1/EmployeeWage.cs
@@ -0,0 +1,17 @@
+﻿
+namespace EmployeeWageComputation
+{
+    public class EmployeeWage
+    {
+        const int IS_PRESENT = 1;
+        public void empAttandence()
+        {
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 1)
+                Console.WriteLine("Employee is Present");
+            else
+                Console.WriteLine("Employee is Absent");
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+using EmployeeWageComputation;
+EmployeeWage attandence = new EmployeeWage();
+attandence.empAttandence();
