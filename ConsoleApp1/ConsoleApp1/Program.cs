f0ff20c700faf4e0de701ae50737bd19200e4b34
snehasonwane <snehasonwane01@gmail.com>
2022-05-01 23:04:23 +0530

    check emp daily wages

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+//using EmployeeWageComputation;
+//EmployeeWage attandence = new EmployeeWage();
+//attandence.empAttandence();
+
 using EmployeeWageComputation;
 EmployeeWage attandence = new EmployeeWage();
 attandence.empAttandence();
+EmployeeWage computation = new EmployeeWage();
+computation.dailyEmpWage();
